f225bb9654deebc6f0cb552971386bc197e3ad09
yc-shawn <ethanloatrchen@gmail.com>
2020-05-02 15:58:10 -0700

    Add home header

---
+++ src/app/home/home-header/home-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yc-home-header',
+  templateUrl: './home-header.component.html',
+  styleUrls: ['./home-header.component.scss']
+})
+export class HomeHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeHeaderComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: HomeComponent }])
