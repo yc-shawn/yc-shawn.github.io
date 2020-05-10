faa3b770414cbe57bfc63ff67ebbb71e8ffaaf61
yc-shawn <ethanloatrchen@gmail.com>
2020-05-10 11:17:38 -0700

    Add skills section

--- src/app/home/home-aboutme/home-aboutme.component.ts
+++ src/app/home/home-aboutme/home-aboutme.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAboutmeComponent implements OnInit {
 
+  skills: string[] = [
+    'Java',
+    'Javascript',
+    'Typescript',
+    'NodeJS',
+    'React/Redux',
+    'Ember',
+    'Angular',
+    'Bootstrap',
+    'Jquery',
+    'CSS/SASS/SCSS',
+    'AWS',
+    'MongoDB'
+  ];
+
+  levels = {
+    'Java': 5,
+    'Python': 4,
+    'C#': 3,
+    'Swift': 4,
+    'C++': 3,
+    'HTML': 5,
+    'CSS/SASS/SCSS': 5,
+    'Javascript': 5,
+    'Typescript': 4,
+    'Jquery': 4,
+    'NodeJS': 4,
+    'React/Redux': 4,
+    'SQL/NoSQL': 3,
+    'Ember': 4,
+    'Angular': 4,
+    'Bootstrap': 4,
+    'AWS': 3,
+    'MongoDB': 3
+  }
+
   constructor() { }
 
   ngOnInit(): void {
